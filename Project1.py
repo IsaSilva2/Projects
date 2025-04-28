9d7a7553ab2a191dd5b263759cabd7febce5a17c
IsaSilva2 <isabelocasilva@gmail.com>
2025-04-28 13:16:39 -0300

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,16 @@
+
+print("Olá. Vamos aprender matemática básica. Mas primeiro, gostaria de saber o seu nome!")
+
+name = input("Digite seu nome aqui: ")
+
+print(f"Bem vinda a aula de matemática, {name}\nComeçaremos a aprender soma, subtração, multiplicação e divisão")
+
+number1 = float(input("Digite um número para começar: "))
+number2 = float(input("Digite um segundo número: "))
+
+soma = number1 + number2
+subtração = number1 - number2
+multiplicação = number1 * number2
+divisão = number1 / number2
+
+print(f"Com os números que você me deu a soma é: {soma}\nA subtração é: {subtração}\nA multiplicação é: {multiplicação}\nA divisão é divisão é: {divisão} ")
